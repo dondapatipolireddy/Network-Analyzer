93f15777988ca9fc20a6ccbfcb77502728c1bb40
dondapatipolireddy <dondapatipolireddy@gmail.com>
2026-05-19 19:14:08 +0530

    database created

--- backend/capture.py
+++ backend/capture.py
@@ -1,9 +1,69 @@
-from scapy.all import sniff,TCP,IP,UDP
-def packet_sniff(packet):
-    if IP in packet:
-        src=packet[IP].src
-        dst=packet[IP].dst
-        proto=packet[IP].proto
-        port=packet[IP].dport if TCP in packet else 0
-        print(f"source: {src} Destination: {dst} Protocol: {proto} port: {port}")
-sniff(prn=packet_sniff,store=False)  #prn=packet_sniff it means capture packet and call packet_sniff function  and (store=False:) not store the capture packets better for live monitoring 
+from scapy.all import sniff,TCP,IP,UDP,ARP,ICMP
+from database import init_db,save_packet,get_top_ips
+import datetime
+def get_protocol(packet):
+    if TCP in packet:
+        port=packet[TCP].dport
+        if port==443 : return "HTTPS" , port
+        if port==80 : return "HTTP" , port
+        if port==22 : return "SSH" , port
+        if port==21 : return "FTP" , port
+        return "TCP" , port
+    if UDP in packet:
+        port=packet[UDP].dport
+        if port==53 : return "DNS" , port
+        return "UDP" ,port
+    if ICMP in packet:
+        return "ICMP" , 0
+    return "OTHER" , 0
+
+def process_packet(packet):
+    try:
+        if ARP in packet:
+            src = packet[ARP].psrc
+            dst = packet[ARP].pdst
+
+            time = datetime.datetime.now().strftime('%H:%M:%S')
+            size = len(packet)
+
+            print(f"time : {time} source: {src} Destination: {dst} Protocol: ARP port: 0 size : {size}")
+
+            save_packet(time, src, dst, "ARP", 0, size)
+
+        elif IP in packet:
+
+            src = packet[IP].src
+            dst = packet[IP].dst
+
+            proto, port = get_protocol(packet)
+
+            time = datetime.datetime.now().strftime('%H:%M:%S')
+            size = len(packet)
+
+            print(f"time : {time} source: {src} Destination: {dst} Protocol: {proto} port: {port} size : {size}")
+
+            save_packet(time, src, dst, proto, port, size)
+
+    except Exception as e:
+        print("Error:", e)
+
+ #prn=packet_sniff it means capture packet and call packet_sniff function  and (store=False:)
+ #  not store the capture packets better for live monitoring 
+
+def show_top_ips():
+    print("\n--- TOP 5 MOST ACTIVE IPs ---")
+    for ip, count in get_top_ips():
+        print(f" ip :{ip} count: {count} packets")
+    print("-----------------------------\n")
+
+init_db()
+print("Capturing packets... Press Ctrl+C to stop")
+print("-" * 65)
+
+try:
+    sniff(prn=process_packet, store=False)
+except KeyboardInterrupt:
+    pass
+finally:
+    print("\nStopped.")
+    show_top_ips()
--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,49 @@
- 
+from pymongo import MongoClient
+import datetime
+
+client=MongoClient('mongodb://localhost:27017/')
+db=client['Network_Analyzer']
+packets_collection=db['packets']
+alerts_collection=db['alerts']
+
+def init_db():
+    print("MongoDB connected. Database ready.")
+
+def save_packet(timestamp,src_ip,dest_ip,proto,port,size):
+    try:
+        packets_collection.insert_one(
+            {
+                'timestamp':timestamp,
+                'src_ip':src_ip,
+                'dest_ip':dest_ip,
+                'proto':proto,
+                'port':port,
+                'size':size,
+                'created_at': datetime.datetime.now()
+            }
+        )
+    except Exception as e:
+        print("MongoDB Error:", e)
+
+def get_top_ips():
+    pipeline=[
+        {'$group': {'_id': '$src_ip', 'count' : { '$sum': 1}}},
+        {'$sort' : {'count' : -1}},
+        {'$limit': 5}
+    ]
+    results=list(packets_collection.aggregate(pipeline))
+    return [(r['_id'],r['count']) for r in results]
+
+def get_recent_packets(limit=100):
+    packets=list(packets_collection.find({},{'_id': 0}).sort('created_at', -1).limit(limit))  
+    # find( filter,projection )  filter means based on condition show like 'protocol':'HTTPS'
+    # projection means which show which now show if "0" not shown if "1" it shown
+    return packets
+
+def get_protocol_status():
+    pipeline=[
+        {'$group': {'_id':'$proto', 'count' : {'$sum':1}}},
+        {'$sort' : {'count' : -1}}
+    ]
+    return list(packets_collection.aggregate(pipeline))
+
