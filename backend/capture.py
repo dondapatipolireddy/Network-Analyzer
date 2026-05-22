e98e0c2f5d422a930210ccffa743a65001c2d00f
dondapatipolireddy <dondapatipolireddy@gmail.com>
2026-05-22 19:24:38 +0530

    added arp_proofing and syn_flooding

--- backend/capture.py
+++ backend/capture.py
@@ -1,5 +1,6 @@
 from scapy.all import sniff,TCP,IP,UDP,ARP,ICMP
 from database import init_db,save_packet,get_top_ips
+from detector import check_port_scan,syn_flood_scan,check_arp_spoof
 import datetime
 def get_protocol(packet):
     if TCP in packet:
@@ -22,12 +23,12 @@ def process_packet(packet):
         if ARP in packet:
             src = packet[ARP].psrc
             dst = packet[ARP].pdst
-
+            src_mac=packet[ARP].hwsrc
             time = datetime.datetime.now().strftime('%H:%M:%S')
             size = len(packet)
 
             print(f"time : {time} source: {src} Destination: {dst} Protocol: ARP port: 0 size : {size}")
-
+            check_arp_spoof(src_mac,src,time)
             save_packet(time, src, dst, "ARP", 0, size)
 
         elif IP in packet:
@@ -39,6 +40,13 @@ def process_packet(packet):
 
             time = datetime.datetime.now().strftime('%H:%M:%S')
             size = len(packet)
+
+            if TCP in packet:
+                flags = packet[TCP].flags
+                syn_flood_scan(src, str(flags), time)
+
+            if proto in ["TCP", "HTTP", "HTTPS", "SSH", "FTP"]:
+                check_port_scan(src, port, time)
 
             print(f"time : {time} source: {src} Destination: {dst} Protocol: {proto} port: {port} size : {size}")
 
--- backend/detector.py
+++ backend/detector.py
@@ -5,15 +5,20 @@ from database import save_alert
 port_scan_tracking=defaultdict(set)
 syn_flood_tracking=defaultdict(int)
 time_tracking=defaultdict(str)
+syn_time_tracking=defaultdict(str)
+arp_spoof_scanning=defaultdict(set)
 
 PORTSCAN_THRESHOLD=15
 TIME_WINDOW=5
+SYN_WINDOW=10
+SYN_THRESHOLD=100
+ARP_THRESHOLD=3
 
 def check_port_scan(src_ip,dest_port,timestamp):
     now=datetime.datetime.now()
     if src_ip in time_tracking:
         first_seen=time_tracking[src_ip]
-        diff=now-first_seen
+        diff=(now-first_seen).seconds
         if diff>TIME_WINDOW:
             port_scan_tracking[src_ip].clear()
             time_tracking[src_ip]=now
@@ -23,7 +28,39 @@ def check_port_scan(src_ip,dest_port,timestamp):
     port_scan_tracking[src_ip].add(dest_port)
 
     if len(port_scan_tracking)>=PORTSCAN_THRESHOLD:
-        alert_msg = f"PORT SCAN DETECTED from {src_ip} — hit {len(port_scan_tracker[src_ip])} ports"
+        alert_msg = f"PORT SCAN DETECTED from {src_ip} — hit {len(port_scan_tracking[src_ip])} ports"
         print(f"\n🚨 ALERT: {alert_msg}\n")
         save_alert(timestamp, "PORT_SCAN", src_ip, "HIGH", alert_msg)
         port_scan_tracking[src_ip].clear()  # reset after alert
+
+
+#SYN FLOOD ATTACK
+def syn_flood_scan(src_ip,flags,timestamp):
+    if flags!='S':         #in packet having syn bit ==1 then only take packet 
+        return                   #otherwise leaveit
+         
+    now=datetime.datetime.now()
+    if src_ip in syn_flood_tracking:
+        first_seen=syn_flood_tracking[src_ip]
+        diff=(now-first_seen).seconds
+        if diff>SYN_WINDOW:
+            syn_flood_tracking[src_ip]=0
+            syn_time_tracking[src_ip]=now
+    else:
+        syn_time_tracking[src_ip]=now
+    syn_flood_tracking[src_ip]+=1
+
+    if syn_flood_tracking[src_ip]>=SYN_THRESHOLD:
+        alert_msg = f"SYN FLOOD from {src_ip} — {syn_flood_tracking[src_ip]} SYNs in {SYN_WINDOW}s"
+        print(f"\n🚨 [HIGH]   {alert_msg}\n")
+        save_alert(timestamp, "SYN_FLOOD", src_ip, "HIGH", alert_msg)
+        syn_flood_tracking[src_ip] = 0
+
+def check_arp_spoof(src_ip,mac,timestamp):
+    arp_spoof_scanning[mac].add(src_ip)
+
+    if len(arp_spoof_scanning[mac])>=ARP_THRESHOLD:
+        alert_msg = f"ARP SPOOF — MAC {mac} claiming {len(arp_spoof_scanning[mac])} different IPs: {arp_spoof_scanning[mac]}"
+        print(f"\n🚨 [MEDIUM] {alert_msg}\n")
+        save_alert(timestamp, "ARP_SPOOF", mac, "MEDIUM", alert_msg)
+        arp_spoof_scanning[mac].clear()
